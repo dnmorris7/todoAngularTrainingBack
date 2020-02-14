c3784802bc071695e71ff62fb64b19fd1179f6bb
David Morris <dnmorris7@gmail.com>
2020-02-13 23:18:06 -0500

    Basic Authentication Settings

--- restful-web-services/src/main/java/com/davidium/rest/webservices/restfulwebservices/helloworld/BasicAuthenticationController.java
+++ restful-web-services/src/main/java/com/davidium/rest/webservices/restfulwebservices/helloworld/BasicAuthenticationController.java
@@ -7,8 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins="http://localhost:4200")
+
 @RestController
+@CrossOrigin(origins="http://localhost:4200")
 public class BasicAuthenticationController {
 
 	@GetMapping(path = "/basicauth")
